f1346430d92c9ccdf9dbb6177fe409152bec7253
Agas Pratama <165492024+agasekode@users.noreply.github.com>
2025-01-28 20:12:22 +0700

    Add files via upload

---
+++ episode_4_variabel/script.js
@@ -0,0 +1,17 @@
+// Ini merupakan Komen
+// Javascript merupakann bahasa pemograman yang
+
+// Ini merupakan variabel var (Yang sudah tidak digunakan di zaman sekarang)
+
+// var namaDepan = "Ucup";
+// console.log(namaDepan);
+
+// Kekurangan dari variabel var
+// var namaBelakang = "Ganteng";
+// {
+//     var namaBelakang = "Ganteng";
+//     console.log(namaBelakang);
+// }
+// console.log(namaBelakang);
+
+// Jika var yang diluar scoop di hilangkan maka var yang ada di dalam scoop dianggap var global
