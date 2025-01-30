350f6b0b70e15427a6a0b986e89ecaef20ecbf63
tsukihimp4 <agasskuy@gmail.com>
2025-01-30 21:53:49 +0700

    Episode 6

--- episode_4_variabel/script.js
+++ episode_4_variabel/script.js
@@ -1,9 +1,11 @@
 // Ini merupakan Komen
-// Javascript merupakann bahasa pemograman yang
+// Javascript merupakann bahasa pemograman yang membaca nya secara baris per baris dari atas kode hingga akhir kode
 
 // Ini merupakan variabel var (Yang sudah tidak digunakan di zaman sekarang)
 
+// 1. variabel dengan var
 // var namaDepan = "Ucup";
+// Tampilkan data
 // console.log(namaDepan);
 
 // Kekurangan dari variabel var
@@ -15,3 +17,30 @@
 // console.log(namaBelakang);
 
 // Jika var yang diluar scoop di hilangkan maka var yang ada di dalam scoop dianggap var global
+
+// 2. variabel dengan let
+
+let namaDepan = "Gotoh";
+
+console.log(namaDepan);
+
+let namaBelakang = "Futari";
+{
+    let namaBelakang = "Hitori";
+    console.log(namaBelakang);
+}
+console.log(namaBelakang);
+
+// outpunya
+/*
+Gotoh
+Hitori
+Futari
+*/
+
+// 3. variabel dengan const
+
+// Const merupakan variabel yang tidak akan bisa di ubah nilai variabelnya
+
+const UserName = "Kessoku";
+console.log(UserName);
