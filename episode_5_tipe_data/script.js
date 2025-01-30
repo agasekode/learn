0b50002c9935cc7ee857523fcf80c2c3a1598402
tsukihimp4 <agasskuy@gmail.com>
2025-01-30 21:28:34 +0700

    Episode 5

---
+++ episode_5_tipe_data/script.js
@@ -0,0 +1,25 @@
+// Tipe data di JavaScript
+
+// String
+let dataString = "Ucup";
+console.log(dataString);
+console.log(typeof dataString);
+
+// Integer
+let dataNumber = 25;
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+// Boolean
+let dataBoolean = true;
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+// Undefined
+let dataKosong;
+console.log(dataKosong);
+console.log(typeof dataKosong);
+// Jika di isi dengan nilai
+dataKosong = "Nilai"
+console.log(dataKosong);
+console.log(typeof dataKosong);
