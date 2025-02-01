4d394467c9e004d5be2a0593842e8ebfb2e41525
tsukihimp4 <agasskuy@gmail.com>
2025-02-01 13:54:41 +0700

    Sesi 1

---
+++ 1.Pengenalan/script.js
@@ -0,0 +1,6 @@
+console.log(`Hello!`);
+// prompt(`Ini Propmt`);
+alert(`Ini Alert!`)
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myParagraph").textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil officia animi laboriosam ipsum.`;
