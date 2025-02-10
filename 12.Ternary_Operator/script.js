1628af711c2809570fa5238d2f48e4ad4748f89a
tsukihimp4 <agasskuy@gmail.com>
2025-02-10 22:22:27 +0700

    Materi Baru

---
+++ 12.Ternary_Operator/script.js
@@ -0,0 +1,19 @@
+// Ternary Operator = a shortcut to if{} and else{} statments
+//                    help to assgin a variable based on condition
+//                    condition ? codeIfTrue : codeIfFalse;
+
+// let umur = 21;
+// let message = umur >= 18 ? "Kamu orang dewasa!" : "Kamu masih dibawah umur!"
+// console.log(message)
+
+// let time = 9;
+// let message = time < 12 ? "Good Morning!" : "Good Afternoon";
+// console.log(message); 
+
+// let isStudent = false;
+// let message = isStudent ? "Kamu seorang pelajar!" : "Kamu bukan seorang pelajar!";
+// console.log(message);
+
+let purchaseAmount = 99;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`total mu adalah ${purchaseAmount - purchaseAmount * (discount )}`);
---
+++ 13.Switches/script.js
@@ -0,0 +1,7 @@
+// Switch = can be a efficient replacement to many if else statments
+
+let testScore = 92;
+let letterGrade;
+
+switch(true) {
+}
